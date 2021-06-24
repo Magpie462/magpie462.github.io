43de8fc1f65068318783c2c359e4419cb8cae4d5
Magpie462 <85455317+Magpie462@users.noreply.github.com>
2021-06-24 15:44:25 -0500

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -46,7 +46,7 @@ export var soundArray = []; //list of sounds loaded
 export var wallSound = new soundFile("creepy-sound.mp3"); //load sound
 soundArray.push(wallSound); //add sound to list of sounds
 
-export var paddleSound = new soundFile("creepy-sound.mp3");
+export var paddleSound = new soundFile("cinematic_reverb.mp3");
 soundArray.push(paddleSound);
 
 export var scoreSound = new soundFile("creepy-sound.mp3");
